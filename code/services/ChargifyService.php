4e5c1366cb6caf989803950186c2e2ae232745a9
ajshort <andrewjshort@gmail.com>
2011-01-21 00:22:31 +1100

    Added ChargifyService::getCastedProductDetails() to convert a ChargifyProduct object into a template-friendly ArrayData object.

--- code/services/ChargifyService.php
+++ code/services/ChargifyService.php
@@ -24,4 +24,44 @@ class ChargifyService {
 		);
 	}
 
+	/**
+	 * Returns a casted array of product details from a {@link ChargifyProduct}
+	 * object.
+	 *
+	 * @param  ChargifyProduct $product
+	 * @return ArrayData
+	 */
+	public function getCastedProductDetails(ChargifyProduct $product) {
+		$link = Controller::join_links(
+			ChargifyConfig::get_url(), 'products', $product->id
+		);
+
+		$currency = ChargifyConfig::get_currency();
+		$price = DBField::create('Money', array(
+			'Amount'   => ((int) $product->price_in_cents) / 100,
+			'Currency' => $currency
+		));
+		$initial = DBField::create('Money', array(
+			'Amount'   => ((int) $product->initial_charge_in_cents) / 100,
+			'Currency' => $currency
+		));
+		$trial = DBField::create('Money', array(
+			'Amount'   => ((int) $product->trial_price_in_cents) / 100,
+			'Currency' => $currency
+		));
+
+		return new ArrayData(array(
+			'Name'              => $product->name,
+			'Family'            => $product->product_family->name,
+			'ChargifyLink'      => $link,
+			'Price'             => $price,
+			'Interval'          => $product->interval,
+			'IntervalUnit'      => $product->interval_unit,
+			'InitialCharge'     => $initial,
+			'TrialPrice'        => $trial,
+			'TrialInterval'     => $product->trial_interval,
+			'TrialIntervalUnit' => $product->trial_interval_unit
+		));
+	}
+
 }
