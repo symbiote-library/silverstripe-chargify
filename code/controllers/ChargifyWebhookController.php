2d917dbf186f35b169a1ba3d71364d2c2348bddd
ajshort <andrewjshort@gmail.com>
2011-01-21 13:01:49 +1100

    Updated the webhook customer getting code to use the customer ID to get the member by.

--- code/controllers/ChargifyWebhookController.php
+++ code/controllers/ChargifyWebhookController.php
@@ -25,12 +25,12 @@ class ChargifyWebhookController extends Controller {
 
 		// Handle a new subscription being created.
 		if ($event == 'signup_success') {
-			$id     = $payload['subscription']['customer']['reference'];
+			$cust   = $payload['subscription']['customer']['id'];
 			$sub    = $payload['subscription']['id'];
 			$email  = $payload['subscription']['customer']['email'];
 			$prod   = $payload['subscription']['product']['id'];
 
-			$member = $this->getMember($id, $email);
+			$member = $this->getMember($cust);
 
 			if (!$member) {
 				return $this->httpError(404, 'Member could not be found.');
@@ -41,13 +41,13 @@ class ChargifyWebhookController extends Controller {
 
 		// Handle subscription upgrades or downgrades.
 		if ($event == 'subscription_product_change') {
-			$id    = $payload['subscription']['customer']['reference'];
+			$cust  = $payload['subscription']['customer']['id'];
 			$sub   = $payload['subscription']['id'];
 			$email = $payload['subscription']['customer']['email'];
 			$prev  = $payload['previous_product']['id'];
 			$curr  = $payload['subscription']['product']['id'];
 
-			$member = $this->getMember($id, $email);
+			$member = $this->getMember($cust);
 
 			if (!$member) {
 				return $this->httpError(404, 'Member could not be found.');
@@ -59,14 +59,14 @@ class ChargifyWebhookController extends Controller {
 
 		// Handle subscriptions ending.
 		if ($event == 'subscription_state_change') {
-			$id     = $payload['subscription']['customer']['reference'];
+			$cust   = $payload['subscription']['customer']['id'];
 			$sub    = $payload['subscription']['id'];
 			$email  = $payload['subscription']['customer']['email'];
 			$prod   = $payload['subscription']['product']['id'];
 			$state  = $payload['subscription']['state'];
 
 			if (in_array($state, array('canceled', 'expired', 'suspended'))) {
-				$member = $this->getMember($id, $email);
+				$member = $this->getMember($cust);
 
 				if (!$member) {
 					return $this->httpError(404, 'Member could not be found.');
@@ -120,37 +120,19 @@ class ChargifyWebhookController extends Controller {
 	}
 
 	/**
-	 * Attempts to get a {@link Member} object by ID, falling back to email and
-	 * syncing with Chargify.
+	 * Returns the {@link Member} object for a chargify customer ID.
 	 *
 	 * @param  int $id
-	 * @param  string $email
 	 * @return Member
 	 */
-	protected function getMember($id, $email) {
-		$member = DataObject::get_by_id('Member', $id);
+	protected function getMember($id) {
+		$link = DataObject::get_one('ChargifyCustomerLink', sprintf(
+			'"CustomerID" = %d', $id
+		));
 
-		// If the two databases have become out of sync, then attempt to get
-		// the member by email, and re-sync.
-		if (!$member) {
-			$member = DataObject::get_one('Member', sprintf(
-				'Email = \'%s\'', Convert::raw2sql($email)
-			));
-
-			if (!$member) {
-				return false;
-			}
-
-			$member->ChargifyID = $id;
-			$member->write();
-
-			$connector = ChargifyService::instance()->getConnector();
-			$customer  = $connector->getCustomerByID($id);
-			$customer->reference = $member->ID;
-			$connector->updateCustomer($customer);
+		if ($link) {
+			return $link->Member();
 		}
-
-		return $member;
 	}
 
 }
